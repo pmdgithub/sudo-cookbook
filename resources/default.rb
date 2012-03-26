55a141a40bd64b8fb53057df03edb3c5bd2b8acc
Jamie van Dyke <jamie@fearoffish.com>
2012-03-26 12:02:34 +0100

    Boolean isn't a ruby class ;-)

--- resources/default.rb
+++ resources/default.rb
@@ -3,7 +3,7 @@ actions :execute
 attribute :command, :kind_of => String, :default => nil
 attribute :user, :kind_of => String, :default => nil
 attribute :group, :kind_of => String, :default => nil
-attribute :simulate_initial_login, :kind_of => Boolean, :default => nil
+attribute :simulate_initial_login, :default => nil
 attribute :cwd, :kind_of => String, :default => nil
 attribute :environment, :kind_of => Hash, :default => nil
 
